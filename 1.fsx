00a12ebfd33e0930c365bf5f86968daefd90a9a3
Thomas Kelly <tjbk123@gmail.com>
2022-12-04 22:30:15 -0800

    Solve day 1

---
+++ 1.fsx
@@ -0,0 +1,45 @@
+open System
+open System.IO
+
+let day1a() =
+    let rec go (maxTotal:int) (currentTotal:int) =
+        let line = Console.ReadLine()
+
+        if line = null then
+            maxTotal
+        elif line.Length > 2 then
+            let newTotal = currentTotal + int line
+
+            if newTotal > maxTotal then
+                go newTotal newTotal
+            else
+                go maxTotal newTotal
+        else
+            go maxTotal 0
+
+    printfn "Max: %d" (go 0 0)
+
+let day1b() =
+    let rec go (totals:seq<int>) (currentTotal:int) =
+        let line = Console.ReadLine()
+
+        if line = null then
+            Seq.append totals (Seq.singleton currentTotal)
+        elif line.Length > 1 then
+            go totals (currentTotal + int line)
+        else
+            go (Seq.append totals (Seq.singleton currentTotal)) 0
+
+    go Seq.empty 0
+        |> Seq.sortDescending
+        |> Seq.take 3
+        |> Seq.sum
+        |> printfn "Top 3: %d"
+
+printfn """Pick part "a" or "b": """
+let response = Console.ReadLine()
+
+if response = "a" then
+    day1a()
+elif response = "b" then
+    day1b()
